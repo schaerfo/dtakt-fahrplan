fd2769584dd13ea0f6fb2bdaba62fcf486bd1ba6
Christian Schärf <11158353+schaerfo@users.noreply.github.com>
2025-01-31 14:17:34 +0100

    🔨 Extract download to separate Makefiles
    This ensures that Docker won't download everything everytime the main Makefile
    is changed.

---
+++ input.mk
@@ -0,0 +1,12 @@
+input_dir := input
+
+input: $(input_dir)/station_location.csv $(input_dir)/Export_fixed.railml.diff $(input_dir)/dtakt-schedule.zip
+
+$(input_dir)/station_location.csv:
+	wget "https://mirror.traines.eu/hafas-ibnr-zhv-gtfs-osm-matching/D_Bahnhof_2020_alle.CSV" -O $@
+
+$(input_dir)/Export_fixed.railml.diff:
+	wget https://gist.github.com/TheMinefighter/1ed90508f3fff466c43869c1b394b243/raw/7e4cb4983c3bf6e5e2b03564cd72a07a9f9c16cc/Export_fixed.railml.diff -O $@
+
+$(input_dir)/dtakt-schedule.zip:
+	wget "https://fragdenstaat.de/files/foi/519018/3.%20Gutachterentwurf%20Zielfahrplan%20Dtakt%20-%20maschienenlesbar.zip?download" -O $@
