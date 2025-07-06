f8c3a33b6f2952166defc7d1ca9a28acb2b1d791
Christian Schärf <11158353+schaerfo@users.noreply.github.com>
2025-07-06 16:50:32 +0200

    🐛 Discard stations outside of Europe

--- merge_station_location.py
+++ merge_station_location.py
@@ -11,6 +11,13 @@ def main():
     osm_locations_df = pd.read_csv(sys.argv[2])
     osm_locations_df.rename(columns={'name': 'name_db'}, inplace=True)
     osm_locations_df = osm_locations_df[["DS100", "name_db", "Laenge", "Breite"]].dropna(subset=['Laenge'])
+    # Discard locations that are outside Europe
+    osm_locations_df = osm_locations_df[
+        (34.0 <= osm_locations_df['Breite']) &
+        (osm_locations_df['Breite'] <= 72.0) &
+        (-25.0 <= osm_locations_df['Laenge']) &
+        (osm_locations_df['Laenge'] <= 45.0)
+    ]
     osm_locations_df.set_index("DS100", inplace=True)
     
     with sqlite3.connect(sys.argv[1]) as conn:
